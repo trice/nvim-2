8e81df21c99bf72322c900d0e3830550c403099e
Terry Rice <trice7454@gmail.com>
2025-08-02 21:53:05 -0500

    Forgot which-key

--- init.lua
+++ init.lua
@@ -414,6 +414,24 @@ require("lazy").setup({
                 })
             end
         },
+        {
+            "folke/which-key.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            },
+            keys = {
+                {
+                    "<leader>?",
+                    function()
+                        require("which-key").show({ global = false })
+                    end,
+                    desc = "Buffer Local Keymaps (which-key)",
+                },
+            },
+        },
         -- import your plugins
         -- { import = "plugins" },
     },
